69667ed2f3c0cdc8e2643310d759474ed1b83737
Honeybunch <amt3824@g.rit.edu>
2023-02-07 16:49:40 -0800

    Some more cleanup

--- toybox/cmake.py
+++ toybox/cmake.py
@@ -1,20 +1,36 @@
 import bpy
 import os
 import subprocess
+import threading
+
+
+def run_build(context):
+    abs_path = os.path.abspath(context.scene.toybox.project_path)
+    config = context.scene.toybox.build_config
+    preset = context.scene.toybox.build_preset
+
+    subprocess.run("cmake --preset " + preset, cwd=abs_path)
+    subprocess.run(
+        "cmake --build --preset " + config + "-" + preset, cwd=abs_path)
 
 
 class BuildOperator(bpy.types.Operator):
     bl_idname = "tb.build"
     bl_label = "Build"
+    bl_options = {'INTERNAL'}
 
     def execute(self, context):
-        abs_path = os.path.abspath(context.scene.toybox.project_path)
-        config = context.scene.toybox.build_config
-        preset = context.scene.toybox.build_preset
+        context.window_manager.modal_handler_add(self)
 
-        subprocess.run("cmake --preset " + preset, cwd=abs_path)
-        subprocess.run(
-            "cmake --build --preset " + config + "-" + preset, cwd=abs_path)
+        self.thread = threading.Thread(target=run_build, args=(context,))
+        self.thread.start()
+
+        return {'RUNNING_MODAL'}
+
+    def modal(self, context, event):
+        if self.thread.is_alive():
+            return {'PASS_THROUGH'}
+
         return {'FINISHED'}
 
 
@@ -28,7 +44,20 @@ class RunOperator(bpy.types.Operator):
         preset = context.scene.toybox.build_preset
         preset_opts = preset.split('-')
         arch = preset_opts[0]
-        plat = preset_opts[1]
+        plat = ''
+        if 'windows-ninja' in preset:
+            plat = 'windows'
+        elif 'windows-static-ninja' in preset:
+            plat = 'windows-static'
+        elif 'mingw-ninja' in preset:
+            plat = 'mingw'
+        elif 'mingw-static-ninja' in preset:
+            plat = 'mingw-static'
+        elif 'windows-vs2022' in preset:
+            plat = 'windows-clangcl'
+        elif 'windows-static-vs2022' in preset:
+            plat = 'windows-static-clangcl'
+
         project_name = context.scene.toybox.project_name
 
         exe_path = abs_path + "/build/"+arch+"/"+plat+"/" + config
